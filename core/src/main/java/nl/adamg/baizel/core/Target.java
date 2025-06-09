6753e7f12b6f62029455333f6372074ec9193670
Avallach <contact-github@avallach.ovh>
2025-06-09 07:32:56 +0200

    model target and task

--- core/src/main/java/nl/adamg/baizel/core/Target.java
+++ core/src/main/java/nl/adamg/baizel/core/Target.java
@@ -1,8 +1,16 @@
 package nl.adamg.baizel.core;
 
-public class Target {
+import nl.adamg.baizel.internal.common.util.collections.EntityComparator;
+
+import javax.annotation.CheckForNull;
+
+/// Format: `[@[<ORG>/]<MODULE>][//<PATH>][:<TARGET_NAME>]`
+/// Example: `@foo/bar//baz/qux:main`
+/// Example: `//baz/qux`
+public class Target implements Comparable<Target> {
     private final nl.adamg.baizel.core.entities.Target entity;
 
+    //region value-like type
     @Override
     public String toString() {
         var sb = new StringBuilder();
@@ -18,6 +26,40 @@ public class Target {
         }
         return sb.toString();
     }
+
+    @Override
+    public int compareTo(Target that) {
+        return EntityComparator.compareBy(
+                this, that,
+                Target::organization,
+                Target::module,
+                Target::path,
+                Target::targetName
+        );
+    }
+    //endregion
+
+    //region getters
+    @CheckForNull
+    public String organization() {
+        return entity.organization;
+    }
+
+    @CheckForNull
+    public String module() {
+        return entity.module;
+    }
+
+    @CheckForNull
+    public String path() {
+        return entity.path;
+    }
+
+    @CheckForNull
+    public String targetName() {
+        return entity.targetName;
+    }
+    //endregion
 
     //region generated code
     public Target(nl.adamg.baizel.core.entities.Target entity) {
