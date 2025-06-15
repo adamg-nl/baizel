bd9a5edea9bdb063e74b413e9dfe04b4676162f5
Avallach <contact-github@avallach.ovh>
2025-06-15 11:51:45 +0200

    add project root

--- core/src/main/java/nl/adamg/baizel/core/Project.java
+++ core/src/main/java/nl/adamg/baizel/core/Project.java
@@ -1,17 +1,23 @@
 package nl.adamg.baizel.core;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Objects;
 
 public class Project {
     private final nl.adamg.baizel.core.entities.Project entity;
 
     public static Path findProjectRoot(Path path) {
-        return path; // fall back to considering given directory as implicit project root
+        var root = path;
+        while (root != null && !Files.exists(root.resolve("project-info.java"))) {
+            root = root.getParent();
+        }
+        // fall back to considering given directory as implicit project root
+        return Objects.requireNonNullElse(root, path);
     }
 
-    public static Project load(Path path) {
-        var root = path;
-        return new Project(new nl.adamg.baizel.core.entities.Project());
+    public static Project load(Path root) {
+        return new Project(new nl.adamg.baizel.core.entities.Project(root));
     }
 
     //region generated code
